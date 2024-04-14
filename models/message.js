b0477d18919c1abdfae7af397afce35aad9d9b2e
minhaj-boop <imminhajj@gmail.com>
2024-04-14 12:59:49 +0600

    changes

--- models/message.js
+++ models/message.js
@@ -4,6 +4,7 @@ const messageSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true },
     phone: { type: String, required: true },
+    date: { type: String, required: true },
     message: { type: String, required: true }
 });
 
