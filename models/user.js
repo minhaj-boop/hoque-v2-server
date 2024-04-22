4c382064f89c5732ca7ddb75da828e2edf83f28c
minhaj-boop <imminhajj@gmail.com>
2024-04-22 20:31:43 +0600

    jwt time changed

--- models/user.js
+++ models/user.js
@@ -11,7 +11,7 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.methods.generateAuthToken = function () {
-    const token = jwt.sign({ _id: this._id }, process.env.JWTPRIVATEKEY, { expiresIn: "7d" })
+    const token = jwt.sign({ _id: this._id }, process.env.JWTPRIVATEKEY, { expiresIn: "1d" })
     return token
 };
 
