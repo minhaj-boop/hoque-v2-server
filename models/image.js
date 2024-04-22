820c8225624d0a31862e72f552fc555bb1af919c
minhaj-boop <imminhajj@gmail.com>
2024-04-22 20:24:23 +0600

    image route change

--- models/image.js
+++ models/image.js
@@ -4,6 +4,7 @@ const imageSchema = new mongoose.Schema({
     title: { type: String, required: true },
     desc: { type: String, required: true },
     url: { type: String, required: true },
+    category: { type: String, required: true },
 });
 
 const Image = mongoose.model("image", imageSchema)
