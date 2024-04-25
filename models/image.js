93a270222374974f988b010d807670b8a84f4364
minhaj-boop <imminhajj@gmail.com>
2024-04-25 14:04:48 +0600

    blurhash string field

--- models/image.js
+++ models/image.js
@@ -4,6 +4,7 @@ const imageSchema = new mongoose.Schema({
     title: { type: String, required: true },
     desc: { type: String, required: true },
     url: { type: String, required: true },
+    blurhash: { type: String, required: true },
     category: { type: String, required: true },
 });
 
