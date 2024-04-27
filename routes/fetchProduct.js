183ae525c3b4bdca808669d774ce88b165855b06
minhaj-boop <imminhajj@gmail.com>
2024-04-28 05:10:04 +0600

    ...

--- routes/fetchProduct.js
+++ routes/fetchProduct.js
@@ -1,6 +1,5 @@
 const router = require("express").Router()
 const { Image } = require('../models/image')
-const { ObjectId } = require('mongodb');
 
 router.get("/:id", async (req, res) => {
 
@@ -8,8 +7,8 @@ router.get("/:id", async (req, res) => {
         const product = await Image.findOne({ _id: req.params.id })
         res.send({ status: "ok", data: product })
     } catch (error) {
-        console.error('Error fetching Images:', error);
-        res.status(400).json(req.params.id + "......");
+        // console.error('Error fetching Images:', error);
+        res.status(400).json({ error: "Internal server error" });
     }
 })
 
