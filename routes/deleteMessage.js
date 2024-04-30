a614206df2f086f3c6b9c4ce2a91381e731945c0
minhaj-boop <imminhajj@gmail.com>
2024-05-01 00:22:01 +0600

    delete user

--- routes/deleteMessage.js
+++ routes/deleteMessage.js
@@ -1,19 +1,22 @@
-const router = require("express").Router()
-const { Message } = require('../models/message')
+// 
 
-router.post("/", async (req, res) => {
-    const { message } = req.body
-    // console.log(message)
+const router = require("express").Router();
+const { Message } = require('../models/message');
+
+router.delete("/:id", async (req, res) => {
+    const messageId = req.params.id;
+    // console.log(messageId); // Ensure you get the correct message ID
+
     try {
-        Message.deleteOne(
-            { _id: message }, function (err, res) {
-                console.log(err);
-            }
-        )
-        res.send({ status: "ok", data: "Deleted" })
+        const deletedMessage = await Message.findByIdAndDelete(messageId);
+        if (!deletedMessage) {
+            return res.status(404).json({ status: "error", message: "Message not found" });
+        }
+        res.status(200).json({ status: "ok", data: "Message deleted successfully" });
     } catch (error) {
-        console.log(error);
+        console.error("Error deleting message:", error);
+        res.status(500).json({ status: "error", message: "Internal server error" });
     }
-})
+});
 
-module.exports = router
+module.exports = router;
